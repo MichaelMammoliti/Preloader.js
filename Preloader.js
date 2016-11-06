33d4dd526e0e99524a0681e71c00791a266ccd52
Michael Mammoliti <mammoliti.michael@gmail.com>
2016-11-06 18:28:19 +0000

    Update Preloader.js

--- Preloader.js
+++ Preloader.js
@@ -1,8 +1,8 @@
 var Loader = (function(options){
 
-	var filesLen 		= options.files.length,
-			filesLoaded = 0,
-			currentFile = 0
+	var 	filesLen 		= options.files.length,
+				filesLoaded = 0,
+				currentFile = 0
 	;
 
 	function init()
