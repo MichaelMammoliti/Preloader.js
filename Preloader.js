13c7a44bd489880f2a21a20971af19da317e8536
Michael Mammoliti <mammoliti.michael@gmail.com>
2016-11-09 23:39:15 +0000

    Update Preloader.js

--- Preloader.js
+++ Preloader.js
@@ -1,157 +1,157 @@
 /*
-	Script info:
-	================
-	- Author: Michael Mammoliti
-	- Version: 1.0.0
-	- Release date: 06 November 2016
-	- GitHub: https://github.com/MichaelMammoliti/Preloader.js/
+  Script info:
+  ================
+  - Author: Michael Mammoliti
+  - Version: 1.0.0
+  - Release date: 06 November 2016
+  - GitHub: https://github.com/MichaelMammoliti/Preloader.js/
 
-	Contact info
-	================
-	- GitHub: https://github.com/MichaelMammoliti
-	- Mail: mammoliti.michael@gmail.com
-	- Twitter: @MichMammoliti
+  Contact info
+  ================
+  - GitHub: https://github.com/MichaelMammoliti
+  - Mail: mammoliti.michael@gmail.com
+  - Twitter: @MichMammoliti
 
-	License Info
-	================
-	- Released under the MIT license.
+  License Info
+  ================
+  - Released under the MIT license.
 */
 
 
 var Loader = (function(options){
 
-	var 	filesLen 		= options.files.length,
-				filesLoaded = 0,
-				currentFile = 0
-	;
+  var   filesLen    = options.files.length,
+        filesLoaded = 0,
+        currentFile = 0
+  ;
 
-	function init()
-	{
-		if( !options.path || !checkOptions() || !checkFiles() ) return;
+  function init()
+  {
+    if( !options.path || !checkOptions() || !checkFiles() ) return;
 
-		load();
-	}
+    load();
+  }
 
-	function checkOptions()
-	{
-		if(!options) return false;
+  function checkOptions()
+  {
+    if(!options) return false;
 
-		if(!options.callbacks)
-		{
-			options.callbacks = {
-				onLoadComplete: function(){},
-				onLoadStart: function(){},
-				onLoadEnd: function(){},
-				onLoadAbort: function(){},
-				onLoadError: function(){},
-				onSuccessAll: function(){}
-			};
-		}
+    if(!options.callbacks)
+    {
+      options.callbacks = {
+        onLoadComplete: function(){},
+        onLoadStart: function(){},
+        onLoadEnd: function(){},
+        onLoadAbort: function(){},
+        onLoadError: function(){},
+        onSuccessAll: function(){}
+      };
+    }
 
-		return true;
-	}
+    return true;
+  }
 
-	function checkFiles()
-	{
-		if(!options.files || !options.files.length)
-		{
-			console.error("No files loaded");
-			return false;
-		}
+  function checkFiles()
+  {
+    if(!options.files || !options.files.length)
+    {
+      console.error("No files loaded");
+      return false;
+    }
 
-		return true;
-	}
+    return true;
+  }
 
-	function checkLoadingComplete()
-	{
-		if(filesLoaded !== filesLen) return false;
+  function checkLoadingComplete()
+  {
+    if(filesLoaded !== filesLen) return false;
 
-		if(options.debug) console.info("finish loading 3 files.", options.files);
+    if(options.debug) console.info("finish loading 3 files.", options.files);
 
-		options.callbacks.onSuccessAll();
+    options.callbacks.onSuccessAll();
 
-		return true;
-	}
+    return true;
+  }
 
-	function load()
-	{
+  function load()
+  {
 
-		options.files.forEach(function(obj, index){
-			var image = new Image(),
-					src 	= createURL(obj)
-			;
+    options.files.forEach(function(obj, index){
+      var image = new Image(),
+          src   = createURL(obj)
+      ;
 
-			if(src)
-			{
-				setImageSrc(image, src);
-				addImageEvents(image, src);
-			}
+      if(src)
+      {
+        setImageSrc(image, src);
+        addImageEvents(image, src);
+      }
 
-		});
-	}
+    });
+  }
 
-	function setImageSrc(image, source)
-	{
-		image.src = source;
-	}
+  function setImageSrc(image, source)
+  {
+    image.src = source;
+  }
 
 
-	// Events
-	// ====================================
-	var ev = {
-		loadStart: function(image, source)
-		{
-			if(options.debug) console.info("loading: " + source);
-			options.callbacks.onLoadStart();
-		},
+  // Events
+  // ====================================
+  var ev = {
+    loadStart: function(image, source)
+    {
+      if(options.debug) console.info("loading: " + source);
+      options.callbacks.onLoadStart();
+    },
 
-		load: function(image, source)
-		{
+    load: function(image, source)
+    {
 
-			if(options.debug) console.info("finish loading: " + source);
+      if(options.debug) console.info("finish loading: " + source);
 
-			options.callbacks.onLoadEnd();
+      options.callbacks.onLoadEnd();
 
-			filesLoaded++; // increment file
+      filesLoaded++; // increment file
 
-			checkLoadingComplete();
-		},
+      checkLoadingComplete();
+    },
 
-		loadEnd: function(image, source){},
+    loadEnd: function(image, source){},
 
-		error: function(image, source)
-		{
-			if(options.debug) console.error("Error preloading the file: " + source);
-			options.callbacks.onLoadError(image);
-		},
+    error: function(image, source)
+    {
+      if(options.debug) console.error("Error preloading the file: " + source);
+      options.callbacks.onLoadError(image);
+    },
 
-		abort: function(image, source)
-		{
-			console.warn("Loading has been aborted for: " + source );
-		}
-	};
+    abort: function(image, source)
+    {
+      console.warn("Loading has been aborted for: " + source );
+    }
+  };
 
-	function addImageEvents(image, source)
-	{
-		image.addEventListener("loadstart", function() { ev.loadStart(image, source); });
-		image.addEventListener("load", function() { ev.load(image, source); });
-		image.addEventListener("error", function() { ev.error(image, source); });
-		image.addEventListener("abort", function() { ev.abort(image, source); });
-		image.addEventListener("loadend", function() { ev.loadEnd(image, source); });
-	}
+  function addImageEvents(image, source)
+  {
+    image.addEventListener("loadstart", function() { ev.loadStart(image, source); });
+    image.addEventListener("load", function() { ev.load(image, source); });
+    image.addEventListener("error", function() { ev.error(image, source); });
+    image.addEventListener("abort", function() { ev.abort(image, source); });
+    image.addEventListener("loadend", function() { ev.loadEnd(image, source); });
+  }
 
-	function createURL(obj)
-	{
-		var path = (obj.path || options.path);
+  function createURL(obj)
+  {
+    var path = (obj.path || options.path);
 
-		if(!path || !obj.file) return false;
+    if(!path || !obj.file) return false;
 
-		return path + "/" + obj.file;
-	}
+    return path + "/" + obj.file;
+  }
 
-	return {
-		init: init
-	}
+  return {
+    init: init
+  }
 
 });
 
@@ -159,33 +159,33 @@ var Loader = (function(options){
 
 var loader = new Loader({
 
-	files: [
-		{
-			path: "",
-			file: "collapsed-sprite-trans.png"
-		},
-		{
-			path: "",
-			file: "expanded-copy-1@2x.png"
-		},
-		{
-			path: "",
-			file: "collapsed-sprite.jpg"
-		}
-	],
+  files: [
+    {
+      path: "",
+      file: "collapsed-sprite-trans.png"
+    },
+    {
+      path: "",
+      file: "expanded-copy-1@2x.png"
+    },
+    {
+      path: "",
+      file: "collapsed-sprite.jpg"
+    }
+  ],
 
-	path: "images",
+  path: "images",
 
-	callbacks: {
-		onLoadComplete: function(){},
-		onLoadStart: function(){},
-		onLoadEnd: function(){},
-		onLoadAbort: function(){},
-		onLoadError: function(){},
-		onSuccessAll: function(){ console.log("end task"); }
-	},
+  callbacks: {
+    onLoadComplete: function(){},
+    onLoadStart: function(){},
+    onLoadEnd: function(){},
+    onLoadAbort: function(){},
+    onLoadError: function(){},
+    onSuccessAll: function(){ console.log("end task"); }
+  },
 
-	debug: true
+  debug: true
 
 });
 
